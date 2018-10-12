7e2be9a3c7050e4b856da5fc8a9cd07f85d5a217
Igor V <igorvv.vn@gmail.com>
2018-10-12 06:09:29 +0000

    menu

--- main.py
+++ main.py
@@ -27,20 +27,27 @@ def textMessage(bot,update):
     bot.send_message(chat_id=update.message.chat_id, text = response)
 def sticker(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text = 'Не шли мне стикеры, плз(((')
+    
+    
 
 custom_keyboard = [['top-left', 'top-right'],
                        ['bottom-left', 'bottom-right']]
 reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
-
+def get_menu(bot, update):
+    bot.send_message(chat_id=update.message.chat_id,chat_id=chat_id, 
+                  text="Custom Keyboard Test", 
+                  reply_markup=reply_markup)
 
 
 start_command_handler = CommandHandler('start', startCommand)
 text_message_handler = MessageHandler(Filters.text, textMessage)
 sticker_message_handler = MessageHandler(Filters.sticker, sticker)
+menu_command_handler = CommandHandler('menu',get_menu)
 
 dispatcher.add_handler(start_command_handler)
 dispatcher.add_handler(text_message_handler)
 dispatcher.add_handler(sticker_message_handler)
+dispatcher.add_handler(menu_command_handler)
 
 updater.start_polling(clean=True)
 
