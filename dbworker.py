809425a6dfe2140d77dcb37bd514053b263a0aa3
A <mail@mail>
2018-10-18 23:24:52 +0300

    fix

--- dbworker.py
+++ dbworker.py
@@ -5,6 +5,7 @@ import postgresql
 """
 def get_curr_state(user_id):
     with postgresql.open(config.DB_URL) as db:
+        query = db.query("SELECT * FROM users_state WHERE id = " )
         try:
             return db
 """
