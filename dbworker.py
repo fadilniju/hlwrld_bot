a6ebdaf21c70bb3e8154c8d602242ff61688e275
Igor V <igorvv.vn@gmail.com>
2018-10-29 11:42:06 +0000

    fix

--- dbworker.py
+++ dbworker.py
@@ -11,14 +11,14 @@ def get_curr_state(user_id):
     users_coll = db_con['user_states']
     try:
         return users_coll.find_one({"_id": user_id})['state']
-    except KeyError:
+    except BaseException:
         return config.States.S_START.value
 
 
 def set_curr_state(user_id, value):
     users_coll = db_con['user_states']
     try:
-        users_coll.save({"_id": user_id, "state": value})
+        users_coll.save({"_id'": user_id, "state": value})
         return True
     except BaseException:
         print('An error occured while trying post to db')
