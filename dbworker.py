c73ecd377a353eeff56e649a1d78d3527b935edc
dznchkone <32663529+dznchkone@users.noreply.github.com>
2018-10-29 14:10:53 +0300

    Update dbworker.py

--- dbworker.py
+++ dbworker.py
@@ -9,17 +9,17 @@ db_con = client[db_uri]
 
 def get_curr_state(user_id):
     users_coll = db_con['user_states']
-        try:
-            return users_coll.find_one({"_id": user_id})['state']
-        except KeyError:
-            return config.States.S_START.value
+    try:
+        return users_coll.find_one({"_id": user_id})['state']
+    except KeyError:
+        return config.States.S_START.value
 
 
 def set_curr_state(user_id, value):
     users_coll = db_con['user_states']
-        try:
-            users_coll.insert_one({"_id": user_id, "state": value})
-            return True
-        except:
-            print('An error occured while trying post to db')
-            return False
+    try:
+        users_coll.insert_one({"_id": user_id, "state": value})
+        return True
+    except:
+        print('An error occured while trying post to db')
+        return False
